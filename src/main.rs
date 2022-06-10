b5293ab1a9330cb6c9f43f3b34e5e9e2eb77ffb6
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2022-06-11 00:19:57 +0200

    :sparkles: csv reader/writer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+// TODO quote handling
+
+#[derive(Debug)]
+struct Table {
+    xss: Vec<Row>,
+}
+type Row = Vec<Cell>;
+type Cell = String;
+
+
+impl Table {
+    fn from_csv(csv_str: &str) -> Self {
+        Self {
+            xss: csv_str
+                .split("\n")
+                .map(|line| line.split(",").map(|x| x.to_owned()).collect::<Vec<Cell>>())
+                .collect(),
+        }
+    }
+    fn to_csv(self) -> String {
+        self.xss
+            .iter()
+            .map(|line| line.join(","))
+            .collect::<Vec<String>>()
+            .join("\n")
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
