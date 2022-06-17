07e8969474805abe0f0e437d9c4d4aa017b8ccef
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2022-06-17 16:05:44 +0200

    :green_heart: make it build && clippy

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,32 @@
-// TODO quote handling
+// TODO put csv and md line parser into its own func
 
 use anyhow::{bail, Result};
 
 #[derive(Debug)]
 struct Table {
+    #[allow(dead_code)]
     xss: Vec<Row>,
 }
 type Row = Vec<Cell>;
 type Cell = String;
 
-fn line_is_md_sep(line: &str) -> bool {
-    false
+#[allow(dead_code)]
+fn line_is_md_sep(_line: &str) -> bool {
+    todo!()
 }
 
 impl Table {
+    #[allow(dead_code)]
     pub fn from_csv(csv_str: &str) -> Self {
         Self {
             xss: csv_str
-                .split("\n")
-                .map(|line| line.split(",").map(|x| x.to_owned()).collect::<Vec<Cell>>())
+                .split('\n')
+                .map(|line| line.split(',').map(|x| x.to_owned()).collect::<Vec<Cell>>())
                 .collect(),
         }
     }
-    pub fn to_csv(self) -> String {
+    #[allow(dead_code)]
+    pub fn into_csv(self) -> String {
         self.xss
             .iter()
             .map(|line| line.join(","))
@@ -30,15 +34,16 @@ impl Table {
             .join("\n")
     }
 
+    #[allow(dead_code)]
     pub fn from_md_table(md_table_str: &str) -> Result<String> {
-        let lines: Vec<&str> = md_table_str.split("\n").collect();
+        let lines: Vec<&str> = md_table_str.split('\n').collect();
 
         // check if at least 2 lines
         // (header and seperator)
         if lines.len() < 2 {
             let error_msg = "Markdown table too small to be valid!";
             log::error!("{}", error_msg);
-            bail!(Err(error_msg));
+            bail!(error_msg);
         }
 
         // Check if second line is a seperator
@@ -50,23 +55,25 @@ impl Table {
                 hopefully_sep
             );
             log::error!("{}", error_msg);
-            bail!(Err(error_msg));
+            bail!(error_msg);
         }
 
         // parse header
         // remove first and last "|" since they are a the edge
-        let header: Row = lines.get(0).unwrap()[1..]
-            .split("\n")
+        let _header: Row = lines.get(0).unwrap()[1..]
+            .split('\n')
             .map(|x| x.to_owned())
             .collect();
 
         // parse body
 
-        Ok(String::new())
+        todo!()
     }
 
-    pub fn to_md_table(self) -> String {
-        String::new()
+    // TODO can we use to and ref?
+    #[allow(dead_code)]
+    pub fn into_md_table(self) -> String {
+        todo!()
     }
 }
 
